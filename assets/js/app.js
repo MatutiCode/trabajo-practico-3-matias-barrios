e45a862ac3c6d03b027efedc301351d1c575df78
MatutiCode <matutebarrios2020@gmail.com>
2026-06-08 21:00:00 -0300

    se termino el listado de personajes

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const urlApiGeneral = "https://thesimpsonsapi.com/api/characters";
+const urlApiIndividual = "https://thesimpsonsapi.com/api/characters/1";
+
+let personajes = [];
+
+async function cargarPersonajes() {
+  const response = await fetch(urlApiGeneral);
+  const datos = await response.json();
+  personajes = datos.results;
+  mostrarPersonajes(personajes);
+}
+
+cargarPersonajes();
+
+function mostrarPersonajes(lista) {
+  const contenedor = document.getElementById("contenedor");
+  contenedor.innerHTML = "";
+
+  lista.forEach((personaje) => {
+    // const tarjeta = document.createElement("div");
+    const imagen =
+      "https://cdn.thesimpsonsapi.com/500" + personaje.portrait_path;
+    contenedor.innerHTML += `
+    <div class="card" style="width: 15rem">
+          <img src="${imagen}" class="card-img-top" alt="${personaje.name}">
+          <div class="card-body">
+            <h5 class="card-title">${personaje.name}</h5>
+            <p class="card-text">
+              Ocupación: ${personaje.occupation}
+            </p>
+            <p class="card-text">
+              Estado: ${personaje.status}
+            </p>
+            <button data-id="${personaje.id}" class="btn btn-primary">Ver detalles</button>
+          </div>
+    </div>
+    `;
+
+    // contenedor.appendChild(tarjeta);
+    console.log(imagen);
+  });
+}
